b5c45c6c0aa51c6f6de4b6a71c04f12f61d77dcc
pongsapat64 <114325058+pongsapat64@users.noreply.github.com>
2025-02-10 09:26:16 +0700

    [Update]

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import ProcessingOrder from './pages/ProcessingOrder';
 import Receipt from './pages/Receipt';
 import ProductList from './pages/ProductList';
 import Dashboard from './pages/Dashboard';
+import AddEmployee from './pages/AddEmployee';
+import AddProduct from './pages/AddProduct';
 
 function App() {
   return (
@@ -17,10 +19,12 @@ function App() {
         <Route path="/receipt" element={<Receipt />} />
         <Route path="/products" element={<ProductList />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/add-employee" element={<AddEmployee />} />
+        <Route path="/add-product" element={<AddProduct />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
 }
 
-export default App
+export default App;
